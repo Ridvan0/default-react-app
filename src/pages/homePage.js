85ca772cff55f3656a7dc9c9089e9c4f93d999db
Ridvan <ridvan@gulenayva.com>
2023-03-09 18:47:40 +0300

    react default

---
+++ src/pages/homePage.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class HomePage extends Component{
+    render() {
+        return (
+            <div>
+                <span></span>
+        
+            </div>
+        );
+    }
+}
+
+export default HomePage;
